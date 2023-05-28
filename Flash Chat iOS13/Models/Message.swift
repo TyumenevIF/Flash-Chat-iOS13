0eb9f30bdef646241f963a2cce217bbe446ae270
Ilyas Tyumenev <tumenoff@mail.ru>
2023-05-28 13:32:45 +0300

    Add Model,  customising cells in a tableView using a .xib

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Ilyas Tyumenev on 28.05.2023.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
