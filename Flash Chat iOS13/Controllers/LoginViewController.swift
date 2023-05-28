548aaeb48cbb110d275f2e92362ea6ee873cda0b
Ilyas Tyumenev <tumenoff@mail.ru>
2023-05-28 11:56:56 +0300

    Logging in Existing Users

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
-    
 
     @IBAction func loginPressed(_ sender: UIButton) {
-    }
-    
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+                if let err = error {
+                    print(err.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
+    }    
 }
