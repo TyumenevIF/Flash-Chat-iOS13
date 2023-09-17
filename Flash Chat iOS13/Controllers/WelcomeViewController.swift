e71811b30acbb79c97a3c89edfeb87da3c77b0d4
Ilyas Tyumenev <tumenoff@mail.ru>
2023-09-17 14:26:02 +0300

    Update UI for WelcomeViewController

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 50, weight: .black)
+        label.textColor = UIColor(named: K.BrandColors.blue)
+        label.backgroundColor = .clear
+        label.textAlignment = .natural
+        label.numberOfLines = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
+    lazy var registerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Register", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 30)
+        button.setTitleColor(UIColor(named: K.BrandColors.blue), for: .normal)
+        button.setTitleShadowColor(.black, for: .normal)
+        button.backgroundColor = UIColor(named: K.BrandColors.lighBlue)
+        button.contentHorizontalAlignment = .center
+        button.addTarget(self, action: #selector(registerButtonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    lazy var logInButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 30)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemTeal
+        button.contentHorizontalAlignment = .center
+        button.addTarget(self, action: #selector(logInButtonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .systemBackground
+        setupTitleLabel()
+        setViews()
+        setupConstraints()
+    }
+    
+    // MARK: - Private Methods
+    private func setupTitleLabel() {
         titleLabel.text = ""
         var charIndex = 0.0
         let titleText = "⚡️FlashChat"
@@ -23,5 +65,40 @@ class WelcomeViewController: UIViewController {
             }
             charIndex += 1
         }
+    }
+    
+    private func setViews() {
+        view.addSubview(titleLabel)
+        view.addSubview(registerButton)
+        view.addSubview(logInButton)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 52),
+            titleLabel.trailingAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            titleLabel.heightAnchor.constraint(equalToConstant: 60),
+            
+            registerButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            registerButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            registerButton.heightAnchor.constraint(equalToConstant: 48),
+            registerButton.bottomAnchor.constraint(equalTo: logInButton.topAnchor, constant: -8),
+            
+            logInButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            logInButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            logInButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 8),
+            logInButton.heightAnchor.constraint(equalToConstant: 61)
+            
+        ])
+    }
+    
+    @objc func registerButtonPressed() {
+        print("registerButtonPressed")
+    }
+    
+    @objc func logInButtonPressed() {
+        print("logInButtonPressed")
     }
 }
